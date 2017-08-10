653ff479d80179786439351381c409720e0081eb
wudtichaikarun <595159090004@dpu.ac.th>
2017-08-11 04:57:37 +0700

    create mock-book in shared

--- src/app/component/home/home.component.ts
+++ src/app/component/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MdDialog } from '@angular/material';
-import { ShowMoreDetailComponent } from './show-more-detail/show-more-detail.component'
+import { ShowMoreDetailComponent } from './show-more-detail/show-more-detail.component';
+import { MockData } from '../../shared/mock-data';
 
 @Component({
   selector: 'app-home',
@@ -9,135 +10,8 @@ import { ShowMoreDetailComponent } from './show-more-detail/show-more-detail.com
 })
 export class HomeComponent implements OnInit {
   status: boolean = true;
-  devices: object[] =
-  [
-    {
-    "info": {
-        "ssid": "Romantic wifi",
-        "flash_size": 4194304,
-        "flash_id": "1640c8",
-        "chip_id": "f32c3a",
-        "sdk": "1.5.3(aec24ac9)",
-        "mac": "18:FE:34:F3:2C:3A",
-        "client_id": "15936570",
-        "device_id": "15936570",
-        "prefix": "MARU",
-        "ip": "192.168.1.23",
-        "version": 0.96
-    },
-    "d": {
-        "myName": "RB-001",
-        "millis": 36081,
-        "temperature_c": 0,
-        "humidity_percent_rh": 0,
-        "state": 0,
-        "heap": 41536,
-        "rssi": -58,
-        "counter": 4,
-        "subscription": 1
-    }
-},{
-    "info": {
-        "ssid": "Romantic wifi",
-        "flash_size": 4194304,
-        "flash_id": "1640c8",
-        "chip_id": "f32c3a",
-        "sdk": "1.5.3(aec24ac9)",
-        "mac": "18:FE:34:F3:2C:3A",
-        "client_id": "15936570",
-        "device_id": "15936570",
-        "prefix": "MARU",
-        "ip": "192.168.1.6",
-        "version": 0.96
-    },
-    "d": {
-        "myName": "RB-001",
-        "millis": 36081,
-        "temperature_c": 0,
-        "humidity_percent_rh": 0,
-        "state": 0,
-        "heap": 41536,
-        "rssi": -58,
-        "counter": 4,
-        "subscription": 1
-    }
-},{
-    "info": {
-        "ssid": "Romantic wifi",
-        "flash_size": 4194304,
-        "flash_id": "1640c8",
-        "chip_id": "f32c3a",
-        "sdk": "1.5.3(aec24ac9)",
-        "mac": "18:FE:34:F3:2C:3A",
-        "client_id": null,
-        "device_id": "15936570",
-        "prefix": "MARU",
-        "ip": "192.168.22.33",
-        "version": 0.96
-    },
-    "d": {
-        "myName": "SmartHome-001",
-        "millis": 36081,
-        "temperature_c": 0,
-        "humidity_percent_rh": 0,
-        "state": 0,
-        "heap": 41536,
-        "rssi": -58,
-        "counter": 4,
-        "subscription": 1
-    }
-},{
-    "info": {
-        "ssid": "Romantic wifi",
-        "flash_size": 4194304,
-        "flash_id": "1640c8",
-        "chip_id": "f32c3a",
-        "sdk": "1.5.3(aec24ac9)",
-        "mac": "18:FE:34:F3:2C:3A",
-        "client_id": "15936570",
-        "device_id": "15936570",
-        "prefix": "MARU",
-        "ip": "192.168.1.23",
-        "version": 0.96
-    },
-    "d": {
-        "myName": "RB-002",
-        "millis": 36081,
-        "temperature_c": 0,
-        "humidity_percent_rh": 0,
-        "state": 0,
-        "heap": 41536,
-        "rssi": -58,
-        "counter": 4,
-        "subscription": 1
-    }
-},{
-    "info": {
-        "ssid": "Romantic wifi",
-        "flash_size": 4194304,
-        "flash_id": "1640c8",
-        "chip_id": "f32c3a",
-        "sdk": "1.5.3(aec24ac9)",
-        "mac": "18:FE:34:F3:2C:3A",
-        "client_id": "15936570",
-        "device_id": "15936570",
-        "prefix": "MARU",
-        "ip": "192.168.1.23",
-        "version": 0.96
-    },
-    "d": {
-        "myName": "RB-003",
-        "millis": 36081,
-        "temperature_c": 0,
-        "humidity_percent_rh": 0,
-        "state": 0,
-        "heap": 41536,
-        "rssi": -58,
-        "counter": 4,
-        "subscription": 1
-    }
-}
-  ]
+  devices: object[] = MockData;
+
   constructor(public dialog: MdDialog) { }
 
   ngOnInit() {
---
+++ src/app/shared/mock-data.ts
@@ -0,0 +1,156 @@
+const mockData: object[] =
+[
+    {
+    "info": {
+        "ssid": "Romantic wifi",
+        "flash_size": 4194304,
+        "flash_id": "1640c8",
+        "chip_id": "f32c3a",
+        "sdk": "1.5.3(aec24ac9)",
+        "mac": "18:FE:34:F3:2C:3A",
+        "client_id": "15936570",
+        "device_id": "15936570",
+        "prefix": "MARU",
+        "ip": "192.168.1.23",
+        "version": 0.96
+    },
+    "d": {
+        "myName": "RB-001",
+        "millis": 36081,
+        "temperature_c": 0,
+        "humidity_percent_rh": 0,
+        "state": 0,
+        "heap": 41536,
+        "rssi": -58,
+        "counter": 4,
+        "subscription": 1
+    }
+},{
+    "info": {
+        "ssid": "Romantic wifi",
+        "flash_size": 4194304,
+        "flash_id": "1640c8",
+        "chip_id": "f32c3a",
+        "sdk": "1.5.3(aec24ac9)",
+        "mac": "18:FE:34:F3:2C:3A",
+        "client_id": "15936570",
+        "device_id": "15936570",
+        "prefix": "MARU",
+        "ip": "192.168.1.6",
+        "version": 0.96
+    },
+    "d": {
+        "myName": "RB-001",
+        "millis": 36081,
+        "temperature_c": 0,
+        "humidity_percent_rh": 0,
+        "state": 0,
+        "heap": 41536,
+        "rssi": -58,
+        "counter": 4,
+        "subscription": 1
+    }
+},{
+    "info": {
+        "ssid": "Romantic wifi",
+        "flash_size": 4194304,
+        "flash_id": "1640c8",
+        "chip_id": "f32c3a",
+        "sdk": "1.5.3(aec24ac9)",
+        "mac": "18:FE:34:F3:2C:3A",
+        "client_id": null,
+        "device_id": "15936570",
+        "prefix": "MARU",
+        "ip": "192.168.22.33",
+        "version": 0.96
+    },
+    "d": {
+        "myName": "SmartHome-001",
+        "millis": 36081,
+        "temperature_c": 0,
+        "humidity_percent_rh": 0,
+        "state": 0,
+        "heap": 41536,
+        "rssi": -58,
+        "counter": 4,
+        "subscription": 1
+    }
+},{
+    "info": {
+        "ssid": "Romantic wifi",
+        "flash_size": 4194304,
+        "flash_id": "1640c8",
+        "chip_id": "f32c3a",
+        "sdk": "1.5.3(aec24ac9)",
+        "mac": "18:FE:34:F3:2C:3A",
+        "client_id": "15936570",
+        "device_id": "15936570",
+        "prefix": "MARU",
+        "ip": "192.168.1.23",
+        "version": 0.96
+    },
+    "d": {
+        "myName": "RB-002",
+        "millis": 36081,
+        "temperature_c": 0,
+        "humidity_percent_rh": 0,
+        "state": 0,
+        "heap": 41536,
+        "rssi": -58,
+        "counter": 4,
+        "subscription": 1
+    }
+},{
+    "info": {
+        "ssid": "Romantic wifi",
+        "flash_size": 4194304,
+        "flash_id": "1640c8",
+        "chip_id": "f32c3a",
+        "sdk": "1.5.3(aec24ac9)",
+        "mac": "18:FE:34:F3:2C:3A",
+        "client_id": "15936570",
+        "device_id": "15936570",
+        "prefix": "MARU",
+        "ip": "192.168.1.23",
+        "version": 0.96
+    },
+    "d": {
+        "myName": "RB-003",
+        "millis": 36081,
+        "temperature_c": 0,
+        "humidity_percent_rh": 0,
+        "state": 0,
+        "heap": 41536,
+        "rssi": -58,
+        "counter": 4,
+        "subscription": 1
+    }
+},{
+    "info": {
+        "ssid": "Romantic wifi",
+        "flash_size": 4194304,
+        "flash_id": "1640c8",
+        "chip_id": "f32c3a",
+        "sdk": "1.5.3(aec24ac9)",
+        "mac": "18:FE:34:F3:2C:3A",
+        "client_id": null,
+        "device_id": "15936570",
+        "prefix": "MARU",
+        "ip": "192.168.1.23",
+        "version": 0.96
+    },
+    "d": {
+        "myName": "RB-004",
+        "millis": 36081,
+        "temperature_c": 0,
+        "humidity_percent_rh": 0,
+        "state": 0,
+        "heap": 41536,
+        "rssi": -58,
+        "counter": 4,
+        "subscription": 1
+    }
+}
+  ]
+
+export const MockData = mockData;
