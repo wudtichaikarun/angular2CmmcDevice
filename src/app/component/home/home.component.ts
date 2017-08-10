047cc01f168b2264c7b26ef5d3bc00b92fe5583c
wudtichaikarun <595159090004@dpu.ac.th>
2017-08-10 23:39:25 +0700

    ui status can working

--- src/app/component/home/home.component.ts
+++ src/app/component/home/home.component.ts
@@ -8,46 +8,85 @@ import { ShowMoreDetailComponent } from './show-more-detail/show-more-detail.com
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  status: string = "connect";
-  deveces: object[] = [
+  status: boolean = true;
+  devices: object[] =
+  [
     {
-      deviceName: 'E8p8266',
-      status: 'connect'
-    },{
-      deviceName: 'RasberyPi',
-      status: 'disconnect'
-    },{
-      deviceName: 'Arduino Nano',
-      status: 'disconnect'
-    },{
-      deviceName: 'Arduino Mini',
-      status: 'disconnect'
-    },{
-      deviceName: 'E8p8266',
-      status: 'disconnect'
-    },{
-      deviceName: 'RasberyPi',
-      status: 'disconnect'
-    },{
-      deviceName: 'Arduino Nano',
-      status: 'connect'
-    },{
-      deviceName: 'Arduino Mini',
-      status: 'disconnect'
+    "info": {
+        "ssid": "Romantic wifi",
+        "flash_size": 4194304,
+        "flash_id": "1640c8",
+        "chip_id": "f32c3a",
+        "sdk": "1.5.3(aec24ac9)",
+        "mac": "18:FE:34:F3:2C:3A",
+        "client_id": "15936570",
+        "device_id": "15936570",
+        "prefix": "MARU",
+        "ip": "192.168.1.23",
+        "version": 0.96
     },
-    {
-      deviceName: 'E8p8266',
-      status: 'connect'
-    },{
-      deviceName: 'RasberyPi',
-      status: 'disconnect'
-    },{
-      deviceName: 'Arduino Nano',
-      status: 'disconnect'
-    },{
-      deviceName: 'Arduino Mini',
-      status: 'disconnect'
+    "d": {
+        "myName": "Romantic-001",
+        "millis": 36081,
+        "temperature_c": 0,
+        "humidity_percent_rh": 0,
+        "state": 0,
+        "heap": 41536,
+        "rssi": -58,
+        "counter": 4,
+        "subscription": 1
     }
+},    {
+    "info": {
+        "ssid": "Romantic wifi",
+        "flash_size": 4194304,
+        "flash_id": "1640c8",
+        "chip_id": "f32c3a",
+        "sdk": "1.5.3(aec24ac9)",
+        "mac": "18:FE:34:F3:2C:3A",
+        "client_id": "15936570",
+        "device_id": "15936570",
+        "prefix": "MARU",
+        "ip": "192.168.1.6",
+        "version": 0.96
+    },
+    "d": {
+        "myName": "Romantic-001",
+        "millis": 36081,
+        "temperature_c": 0,
+        "humidity_percent_rh": 0,
+        "state": 0,
+        "heap": 41536,
+        "rssi": -58,
+        "counter": 4,
+        "subscription": 1
+    }
+},{
+    "info": {
+        "ssid": "Romantic wifi",
+        "flash_size": 4194304,
+        "flash_id": "1640c8",
+        "chip_id": "f32c3a",
+        "sdk": "1.5.3(aec24ac9)",
+        "mac": "18:FE:34:F3:2C:3A",
+        "client_id": null,
+        "device_id": "15936570",
+        "prefix": "MARU",
+        "ip": "192.168.22.33",
+        "version": 0.96
+    },
+    "d": {
+        "myName": "SmartHome-001",
+        "millis": 36081,
+        "temperature_c": 0,
+        "humidity_percent_rh": 0,
+        "state": 0,
+        "heap": 41536,
+        "rssi": -58,
+        "counter": 4,
+        "subscription": 1
+    }
+}
   ]
   constructor(public dialog: MdDialog) { }
 
